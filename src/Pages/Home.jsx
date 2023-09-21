a9b54995be79ed85443550a250d38400a31845cd
ashraf <ashrafmohamed1176@gmail.com>
2023-09-21 12:11:38 +0300

    edit home section

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,8 +1,7 @@
 import Projects from "./Projects";
 import Skills from "./Skills";
-import BioAndInfo from "../components/home/Bio&information";
-import InfoAndSocila from "../components/home/Info&social";
-import WorkInfo from "../components/home/WorkInfo";
+import portfolioImage from '../assets/image/cv.jpg'
+import {Link} from "react-router-dom";
 
 export default function Home() {
   return (
@@ -10,9 +9,18 @@ export default function Home() {
     <div className="containerFromCOmp" style={{ minHeight: "calc(100vh)" }}>
         <div className={'bgc'} style={{width: '40%', minHeight: "calc(100vh)", position: 'absolute', zIndex: '0', background: '#011603', top: '0', right: '0'}}></div>
       <div className="homeContent">
-          <BioAndInfo />
-          <InfoAndSocila />
-          <WorkInfo />
+          <div className={'portfolioImage'}>
+          <img src={portfolioImage} />
+              <h2 className={'header-info'}>
+                  <span>> Hello, I'm Ashraf, </span>
+                  front end developer
+              </h2>
+              <p className={'description'}>I am an Egyptian, I have 21 years studying Communication Information Systems, <br/> at DELTA Academy for management and Accounting Information Systems.</p>
+                <div className={'btns'}>
+                    <a target={'_blank'} href={'https://drive.google.com/file/d/18w50KXsLsqp1pCQO8raihGFzbHpuUMyB/view?usp=sharing'} className={'cv'}>cv</a>
+                    <a target={"_blank"} className={'linkedIn active'} href={'https://www.linkedin.com/in/ashraf-mohamed-b931a7236'}>linkedIn</a>
+                </div>
+          </div>
       </div>
     </div>
         <Skills />
