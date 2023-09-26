fce732a38e3eb2d5b1845779128aed4fc1c87b1f
ashraf <ashrafmohamed1176@gmail.com>
2023-09-26 22:25:38 +0300

    add some projects

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,30 +1,77 @@
 import Projects from "./Projects";
-import Skills from "./Skills";
-import portfolioImage from '../assets/image/cv.jpg'
+import portfolioImage from "../assets/image/cv.jpg";
+import {
+    FaBootstrap,
+    FaCss3Alt, FaDiscord, FaFacebook,
+    FaFigma,
+    FaGitAlt,
+    FaGithub,
+    FaHtml5,
+    FaJs,
+    FaReact,
+    FaSass,
+    FaWhatsapp
+} from "react-icons/fa6";
+import React from "react";
 import {Link} from "react-router-dom";
 
 export default function Home() {
   return (
-      <>
-    <div className="containerFromCOmp" style={{ minHeight: "calc(100vh)" }}>
-        <div className={'bgc'} style={{width: '40%', minHeight: "calc(100vh)", position: 'absolute', zIndex: '0', background: '#011603', top: '0', right: '0'}}></div>
-      <div className="homeContent">
-          <div className={'portfolioImage'}>
-          <img src={portfolioImage} />
-              <h2 className={'header-info'}>
-                  <span>> Hello, I'm Ashraf, </span>
-                  front end developer
-              </h2>
-              <p className={'description'}>I am an Egyptian, I have 21 years studying Communication Information Systems, <br/> at DELTA Academy for management and Accounting Information Systems.</p>
-                <div className={'btns'}>
-                    <a target={'_blank'} href={'https://drive.google.com/file/d/18w50KXsLsqp1pCQO8raihGFzbHpuUMyB/view?usp=sharing'} className={'cv'}>cv</a>
-                    <a target={"_blank"} className={'linkedIn active'} href={'https://www.linkedin.com/in/ashraf-mohamed-b931a7236'}>linkedIn</a>
-                </div>
+    <>
+      <div className="containerFromCOmp" style={{ minHeight: "calc(100vh)" }}>
+        <div className="homeContent">
+          <div className="information">
+            <img className="portfolioImage" src={portfolioImage} />
+            <p className={'my-info'}><span>name:</span> ashraf mohamed</p>
+            <p className={'my-info'}><span>age:</span> 21</p>
+            <p className={'my-info'}><span>jop:</span> front end developer</p>
+            <p className={'my-info'}><span>Nationality:</span> Egyptian</p>
+            <p className={'my-info'}><span>Study:</span> Business information systems (BIS)</p>
+            <p className={'my-info'}><span>Study:</span> Egyptian</p>
           </div>
+          <div className={'jop-information'}>
+            <p className={'title'} style={{marginBottom: '15px'}}> <span>></span> front end developer <br/> ( Started in 2022 to far )</p>
+            <p className={'title'}> <span>></span> Study business information systems at DELTA Academy  <br/> ( Started in 2021 to far )</p>
+            <p className={'title'} style={{paddingLeft: '20px', marginTop: '30px'}}> <span className={'turnIcon'}>></span> skills </p>
+            <section className={'Home-skills'}>
+                <div className="box">
+                  <h1 style={{ color: "red" }}> <FaHtml5 />  </h1>
+                </div>
+                <div className="box">
+                  <h1 style={{ color: "blue" }}> <FaCss3Alt /> </h1>
+                </div>
+                <div className="box">
+                  <h1 style={{ color: "yellow" }}> <FaJs />  </h1>
+                </div>
+                <div className="box">
+                  <h1 style={{ color: "blue" }}> <FaReact /> </h1>
+                </div>
+                <div className="box">
+                  <h1 style={{ color: "blue" }}> <FaBootstrap /> </h1>
+                </div>
+                <div className="box">
+                  <h1 style={{ color: "#777" }}> <FaGithub /> </h1>
+                </div>
+                <div className="box">
+                  <h1 style={{ color: "red" }}> <FaGitAlt /> </h1>
+                </div>
+                <div className="box">
+                  <h1 style={{ color: "green" }}> <FaFigma /> </h1>
+                </div>
+                <div className="box">
+                  <h1 style={{ color: "#bf4080" }}> <FaSass /> </h1>
+                </div>
+            </section>
+            <p className={'title'} style={{paddingLeft: '20px', marginTop: '30px', marginBottom: '20px'}}> <span className={'turnIcon'}>></span> Communication </p>
+            <div className={'Communication'}>
+                <a style={{color: 'blue', backgroundColor: '#eee', borderRadius: '50%', lineHeight: '.6', padding: '5px', }} href={'https://www.facebook.com/ASHRAFMoOoHAMED'}><FaFacebook /></a>
+                <a style={{color: '#eee', backgroundColor: 'blue', borderRadius: '50%', lineHeight: '.6', padding: '5px', }} href={''}><FaDiscord /></a>
+                <a style={{color: '#eee', backgroundColor: 'green', borderRadius: '50%', lineHeight: '.6', padding: '5px', }} href={''}><FaWhatsapp /></a>
+            </div>
+          </div>
+        </div>
       </div>
-    </div>
-        <Skills />
-        <Projects />
-      </>
+      <Projects />
+    </>
   );
 }
--- src/Pages/Projects.jsx
+++ src/Pages/Projects.jsx
@@ -5,64 +5,115 @@ import projsearch from "../assets/Projects/search (1).png";
 import pagecustomizer from "../assets/Projects/page customize.png";
 import ecommerce from "../assets/Projects/e-commerce.png";
 import education from "../assets/Projects/Asheducation.png";
-import productManagement from "../assets/Projects/products managment.png"
-import bondi from "../assets/Projects/bondi.png"
-import './projects.css'
+import productManagement from "../assets/Projects/products managment.png";
+import bondi from "../assets/Projects/bondi.png";
+import "./projects.css";
 import ProjectsCard from "../components/ProjectsCard";
-import {FaBootstrap, FaCss3Alt, FaHtml5, FaReact, FaSquareJs} from "react-icons/fa6";
+import {
+  FaBootstrap,
+  FaCss3Alt,
+  FaHtml5,
+  FaReact,
+  FaSquareJs,
+} from "react-icons/fa6";
 
 export default function Projects() {
-  return(
-      <div className="containerFromCOmp" style={{ paddingTop: "40px" }}>
-        <h1 className="mainHeader" style={{ textAlign: "center" }}> projects </h1>
-          <div className="ProjectContent">
+  return (
+    <>
+      <div className="projects">
+      <h1 className="mainHeader" style={{ textAlign: "center" }}>projects</h1>
+        <div className="containerFromCOmp" style={{ paddingTop: "40px" }}>
+      <div className="ProjectContent">
+        <ProjectsCard
+          img={education}
+          title={"education"}
+          HTMlicon={<FaHtml5 />}
+          cssicon={<FaCss3Alt />}
+          src={"https://github.com/ashrafmo-1/ASH-EDUCATION"}
+          live={"https://ashrafmo-1.github.io/ASH-EDUCATION/"}
+        />
 
-            <ProjectsCard img={education} title={'education'} HTMlicon={<FaHtml5 />} cssicon={<FaCss3Alt />}
-                src={'https://github.com/ashrafmo-1/ASH-EDUCATION'}
-                live={'https://ashrafmo-1.github.io/ASH-EDUCATION/'}
-            />
+        <ProjectsCard
+          img={ecommerce}
+          title={"ecommerce"}
+          reactIcon={<FaReact />}
+          cssicon={<FaCss3Alt />}
+          bs={<FaBootstrap />}
+          jsicon={<FaSquareJs />}
+          src="https://github.com/ashrafmo-1/happy-shoping"
+          live="https://happy-shoping-1.web.app/"
+        />
 
-            <ProjectsCard img={ecommerce} title={'ecommerce'} reactIcon={<FaReact/>} cssicon={<FaCss3Alt />} bs={<FaBootstrap />} jsicon={<FaSquareJs />}
-                src='https://github.com/ashrafmo-1/happy-shoping'
-                live='https://happy-shoping-1.web.app/'
-            />
+        <ProjectsCard
+          img={projsearch}
+          title={"search"}
+          HTMlicon={<FaHtml5 />}
+          cssicon={<FaCss3Alt />}
+          src={"https://github.com/ashrafmo-1/search"}
+          live=""
+        />
 
-            <ProjectsCard img={projsearch} title={'search'} HTMlicon={<FaHtml5 />} cssicon={<FaCss3Alt />}
-                src={'https://github.com/ashrafmo-1/search'}
-                live=''
-            />
+        <ProjectsCard
+          img={pagecustomizer}
+          title={"shape is adjustable"}
+          HTMlicon={<FaHtml5 />}
+          cssicon={<FaCss3Alt />}
+          jsicon={<FaSquareJs />}
+          src={"https://github.com/ashrafmo-1/special-webSite-full-responsive"}
+          live={"https://ashrafmo-1.github.io/special-webSite-full-responsive/"}
+        />
 
-            <ProjectsCard img={pagecustomizer} title={'shape is adjustable'} HTMlicon={<FaHtml5 />} cssicon={<FaCss3Alt />} jsicon={<FaSquareJs />}
-                src={'https://github.com/ashrafmo-1/special-webSite-full-responsive'}
-                live={'https://ashrafmo-1.github.io/special-webSite-full-responsive/'}
-            />
+        <ProjectsCard
+          img={projCalc}
+          title={"calculator"}
+          HTMlicon={<FaHtml5 />}
+          cssicon={<FaCss3Alt />}
+          jsicon={<FaSquareJs />}
+          src={"https://github.com/ashrafmo-1/calculator"}
+          live={"https://ashrafmo-1.github.io/calculator/"}
+        />
 
-              <ProjectsCard img={projCalc} title={'calculator'} HTMlicon={<FaHtml5 />} cssicon={<FaCss3Alt />} jsicon={<FaSquareJs />}
-                src={'https://github.com/ashrafmo-1/calculator'}
-                live={'https://ashrafmo-1.github.io/calculator/'}
-            />
+        <ProjectsCard
+          img={projWeather}
+          title={"weather"}
+          HTMlicon={<FaHtml5 />}
+          cssicon={<FaCss3Alt />}
+          jsicon={<FaSquareJs />}
+          src={"https://github.com/ashrafmo-1/weather"}
+          live={"https://ashrafmo-1.github.io/weather/"}
+        />
 
-              <ProjectsCard img={projWeather} title={'weather'} HTMlicon={<FaHtml5 />} cssicon={<FaCss3Alt />} jsicon={<FaSquareJs />}
-                src={'https://github.com/ashrafmo-1/weather'}
-                live={'https://ashrafmo-1.github.io/weather/'}
-            />
+        <ProjectsCard
+          img={projhome}
+          title={"home design"}
+          HTMlicon={<FaHtml5 />}
+          cssicon={<FaCss3Alt />}
+          // src={'https://github.com/ashrafmo-1/weather'}
+          // live={'https://ashrafmo-1.github.io/weather/'}
+        />
 
-              <ProjectsCard img={projhome} title={'home design'} HTMlicon={<FaHtml5 />} cssicon={<FaCss3Alt />}
-                // src={'https://github.com/ashrafmo-1/weather'}
-                // live={'https://ashrafmo-1.github.io/weather/'}
-            />
+        <ProjectsCard
+          img={productManagement}
+          title={"home design"}
+          reactIcon={<FaReact />}
+          cssicon={<FaCss3Alt />}
+          jsicon={<FaSquareJs />}
+          src={"https://github.com/ashrafmo-1/crud-products-app"}
+          live={"https://crud-products-app.firebaseapp.com/"}
+        />
 
-              <ProjectsCard img={productManagement} title={'home design'} reactIcon={<FaReact/>} cssicon={<FaCss3Alt />} jsicon={<FaSquareJs />}
-                src={'https://github.com/ashrafmo-1/crud-products-app'}
-                live={'https://crud-products-app.firebaseapp.com/'}
-            />
-
-              <ProjectsCard img={bondi} title={'home design'} HTMlicon={<FaHtml5 />} cssicon={<FaCss3Alt />} bs={<FaBootstrap />}
-                src={'https://github.com/ashrafmo-1/bondi-site'}
-                live={'https://ashrafmo-1.github.io/bondi-site/'}
-            />
-
-          </div>
+        <ProjectsCard
+          img={bondi}
+          title={"home design"}
+          HTMlicon={<FaHtml5 />}
+          cssicon={<FaCss3Alt />}
+          bs={<FaBootstrap />}
+          src={"https://github.com/ashrafmo-1/bondi-site"}
+          live={"https://ashrafmo-1.github.io/bondi-site/"}
+        />
       </div>
-          )
+    </div>
+    </div>
+</>
+  );
 }
