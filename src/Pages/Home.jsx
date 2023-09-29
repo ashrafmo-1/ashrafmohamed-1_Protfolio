52c3865d428e4c9a80953c705de78aecca5f3e8f
ashraf <ashrafmohamed1176@gmail.com>
2023-09-30 01:48:22 +0300

    add social midea links

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,19 +1,21 @@
 import Projects from "./Projects";
 import portfolioImage from "../assets/image/cv.jpg";
 import {
-    FaBootstrap,
-    FaCss3Alt, FaDiscord, FaFacebook,
-    FaFigma,
-    FaGitAlt,
-    FaGithub,
-    FaHtml5,
-    FaJs,
-    FaReact,
-    FaSass,
-    FaWhatsapp
+  FaBootstrap,
+  FaCss3Alt,
+  FaDiscord,
+  FaFacebook,
+  FaFigma,
+  FaGitAlt,
+  FaGithub,
+  FaHtml5,
+  FaJs,
+  FaReact,
+  FaSass,
+  FaWhatsapp,
 } from "react-icons/fa6";
 import React from "react";
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 
 export default function Home() {
   return (
@@ -22,51 +24,149 @@ export default function Home() {
         <div className="homeContent">
           <div className="information">
             <img className="portfolioImage" src={portfolioImage} />
-            <p className={'my-info'}><span>name:</span> ashraf mohamed</p>
-            <p className={'my-info'}><span>age:</span> 21</p>
-            <p className={'my-info'}><span>jop:</span> front end developer</p>
-            <p className={'my-info'}><span>Nationality:</span> Egyptian</p>
-            <p className={'my-info'}><span>Study:</span> Business information systems (BIS)</p>
-            <p className={'my-info'}><span>Study:</span> Egyptian</p>
+            <p className={"my-info"}>
+              <span>name:</span> ashraf mohamed
+            </p>
+            <p className={"my-info"}>
+              <span>age:</span> 21
+            </p>
+            <p className={"my-info"}>
+              <span>jop:</span> front end developer
+            </p>
+            <p className={"my-info"}>
+              <span>Nationality:</span> Egyptian
+            </p>
+            <p className={"my-info"}>
+              <span>Study:</span> Business information systems (BIS)
+            </p>
+            <p className={"my-info"}>
+              <span>Study:</span> Egyptian
+            </p>
           </div>
-          <div className={'jop-information'}>
-            <p className={'title'} style={{marginBottom: '15px'}}> <span>></span> front end developer <br/> ( Started in 2022 to far )</p>
-            <p className={'title'}> <span>></span> Study business information systems at DELTA Academy  <br/> ( Started in 2021 to far )</p>
-            <p className={'title'} style={{paddingLeft: '20px', marginTop: '30px'}}> <span className={'turnIcon'}>></span> skills </p>
-            <section className={'Home-skills'}>
-                <div className="box">
-                  <h1 style={{ color: "red" }}> <FaHtml5 />  </h1>
-                </div>
-                <div className="box">
-                  <h1 style={{ color: "blue" }}> <FaCss3Alt /> </h1>
-                </div>
-                <div className="box">
-                  <h1 style={{ color: "yellow" }}> <FaJs />  </h1>
-                </div>
-                <div className="box">
-                  <h1 style={{ color: "blue" }}> <FaReact /> </h1>
-                </div>
-                <div className="box">
-                  <h1 style={{ color: "blue" }}> <FaBootstrap /> </h1>
-                </div>
-                <div className="box">
-                  <h1 style={{ color: "#777" }}> <FaGithub /> </h1>
-                </div>
-                <div className="box">
-                  <h1 style={{ color: "red" }}> <FaGitAlt /> </h1>
-                </div>
-                <div className="box">
-                  <h1 style={{ color: "green" }}> <FaFigma /> </h1>
-                </div>
-                <div className="box">
-                  <h1 style={{ color: "#bf4080" }}> <FaSass /> </h1>
-                </div>
+          <div className={"jop-information"}>
+            <p className={"title"} style={{ marginBottom: "15px" }}>
+              {" "}
+              <span>></span> front end developer <br /> ( Started in 2022 to far
+              )
+            </p>
+            <p className={"title"}>
+              {" "}
+              <span>></span> Study business information systems at DELTA Academy{" "}
+              <br /> ( Started in 2021 to far )
+            </p>
+            <p
+              className={"title"}
+              style={{ paddingLeft: "20px", marginTop: "30px" }}
+            >
+              {" "}
+              <span className={"turnIcon"}>></span> skills{" "}
+            </p>
+            <section className={"Home-skills"}>
+              <div className="box">
+                <h1 style={{ color: "red" }}>
+                  {" "}
+                  <FaHtml5 />{" "}
+                </h1>
+              </div>
+              <div className="box">
+                <h1 style={{ color: "blue" }}>
+                  {" "}
+                  <FaCss3Alt />{" "}
+                </h1>
+              </div>
+              <div className="box">
+                <h1 style={{ color: "yellow" }}>
+                  {" "}
+                  <FaJs />{" "}
+                </h1>
+              </div>
+              <div className="box">
+                <h1 style={{ color: "blue" }}>
+                  {" "}
+                  <FaReact />{" "}
+                </h1>
+              </div>
+              <div className="box">
+                <h1 style={{ color: "blue" }}>
+                  {" "}
+                  <FaBootstrap />{" "}
+                </h1>
+              </div>
+              <div className="box">
+                <h1 style={{ color: "#777" }}>
+                  {" "}
+                  <FaGithub />{" "}
+                </h1>
+              </div>
+              <div className="box">
+                <h1 style={{ color: "red" }}>
+                  {" "}
+                  <FaGitAlt />{" "}
+                </h1>
+              </div>
+              <div className="box">
+                <h1 style={{ color: "green" }}>
+                  {" "}
+                  <FaFigma />{" "}
+                </h1>
+              </div>
+              <div className="box">
+                <h1 style={{ color: "#bf4080" }}>
+                  {" "}
+                  <FaSass />{" "}
+                </h1>
+              </div>
             </section>
-            <p className={'title'} style={{paddingLeft: '20px', marginTop: '30px', marginBottom: '20px'}}> <span className={'turnIcon'}>></span> Communication </p>
-            <div className={'Communication'}>
-                <a style={{color: 'blue', backgroundColor: '#eee', borderRadius: '50%', lineHeight: '.6', padding: '5px', }} href={'https://www.facebook.com/ASHRAFMoOoHAMED'}><FaFacebook /></a>
-                <a style={{color: '#eee', backgroundColor: 'blue', borderRadius: '50%', lineHeight: '.6', padding: '5px', }} href={''}><FaDiscord /></a>
-                <a style={{color: '#eee', backgroundColor: 'green', borderRadius: '50%', lineHeight: '.6', padding: '5px', }} href={''}><FaWhatsapp /></a>
+            <p
+              className={"title"}
+              style={{
+                paddingLeft: "20px",
+                marginTop: "30px",
+                marginBottom: "20px",
+              }}
+            >
+              {" "}
+              <span className={"turnIcon"}>></span> Communication{" "}
+            </p>
+            <div className={"Communication"}>
+              <a
+                style={{
+                  color: "blue",
+                  backgroundColor: "#eee",
+                  borderRadius: "50%",
+                  lineHeight: ".6",
+                  padding: "5px",
+                }}
+                href={"https://www.facebook.com/ASHRAFMoOoHAMED"}
+              >
+                <FaFacebook />
+              </a>
+              <a
+                style={{
+                  color: "#eee",
+                  backgroundColor: "blue",
+                  borderRadius: "50%",
+                  lineHeight: ".6",
+                  padding: "5px",
+                }}
+                href={"https://discord.com/users/870623473901371412"}
+              >
+                <FaDiscord />
+              </a>
+              <a
+                style={{
+                  color: "#eee",
+                  backgroundColor: "green",
+                  borderRadius: "50%",
+                  lineHeight: ".6",
+                  padding: "5px",
+                }}
+                href={
+                  "https://api.whatsapp.com/send/?phone=%2B201007481557&text&type=phone_number&app_absent=0"
+                }
+              >
+                <FaWhatsapp />
+              </a>
             </div>
           </div>
         </div>
