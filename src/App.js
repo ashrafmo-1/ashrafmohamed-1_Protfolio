8b62b4ad606b09b5f973135bd2472f41fc7d5601
ashraf mohamed <ashrafmohamed1176@gmail.com>
2023-10-19 20:10:27 +0200

    update all portfolio

--- src/App.js
+++ src/App.js
@@ -1,19 +1,92 @@
-import { Route, Routes } from "react-router-dom";
-import "./App.css";
-import Nav from "./components/Nav";
-import Home from "./Pages/Home";
-import {Contact} from "./Pages/Contact";
-import Footer from "./components/Footer";
-
+import './App.css';
+import my from './assets/image 1.png'
+import figma from './assets/Rectangle 1.png'
+import react from './assets/Rectangle 2.png'
+import js from './assets/Rectangle 6.png'
+import css from './assets/Rectangle 7.png'
+import redux from './assets/Rectangle 8.png'
+import next from './assets/Rectangle 9.png'
+import { FaHtml5 } from "react-icons/fa6";
+import Projects from './pages/projects/Projects';
 function App() {
+
+  window.onload = function() {
+    alert('the site under fix any erroe')
+  }
+
   return (
-    <div className="container">
-      <Nav />
-      <Routes>
-        <Route path="/" element={<Home />}></Route>
-        <Route path="/contact" element={<Contact />}></Route>
-      </Routes>
-        {/*<Footer />*/}
+    <div>
+      <div className='Container'>
+      <header>
+        <h1 className='nav-title'><span>AS</span>hraf mohamed</h1>
+        <nav>
+          <span>Home</span>
+          <span>About</span>
+          <span>call me</span>
+        </nav>
+      </header>
+        <div className='landing'>
+          <div className='head-image'><img className='my-image' src={my} alt='' /></div>
+          <div className='content'>
+            <p className='head-name'>Hello! I Am <span>ashraf mohamed</span></p>
+            <span className='head-jop'>front end developer</span>
+            <p className='head-hent'>Welcome to my business gallery</p>
+          </div>
+        </div>
+        <div className='aboute'>
+          <h1 className='jopTItle'>I'm a Software front end developer.| </h1>
+          <p className='cuurentJop'>Currently, I'm a Software Engineer at</p>
+          <p className='disc'>
+          Software front end developer, functioning in the industry for 1+ years now.
+          I make meaningful and delightful digital products that create an equilibrium
+          between user needs and business goals.
+          </p>
+        </div>
+        <div className='skills'>
+          <h1 className='skillsTitle'>
+            I'm currently looking to join a cross-functional team
+            that values improving people's lives through accessible design
+          </h1>
+          <p>s</p>
+          <div className='skills-boxes'>
+          <div className='box html'>
+              <FaHtml5 />
+            </div>
+            <div className='box'>
+              <img src={css} alt='' />
+            </div>
+            <div className='box'>
+              <img src={js} alt='' />
+            </div>
+            <div className='box'>
+              <img src={react} alt='' />
+            </div>
+            <div className='box'>
+              <img src={figma} alt='' />
+            </div>
+            <div className='box'>
+              <img src={redux} alt='' />
+            </div>
+            <div className='box'>
+              <img src={next} alt='' />
+            </div>
+          </div>
+        </div>
+        <Projects />
+        <div className='contact-me'>
+          <h2 className='contact-title'>Contact</h2>
+          <p className='contact-descreption'>
+          I'm currently looking to join a cross-functional team that values improving people's lives
+          through accessible design. or have a project in mind? Let's connect.
+          </p>
+          <a href='./' className='contactLink'>ashrafmohamed@gmail.com</a>
+        </div>
+        <footer>
+          <p className='footer'>
+            © 2023 ashraf mohamed. All rights reserved.
+          </p>
+        </footer>
+      </div>
     </div>
   );
 }
---
+++ src/pages/projects/Projects.jsx
@@ -0,0 +1,218 @@
+import './projects.css'
+import edu from '../../assets/Projects/Asheducation.png'
+import bondi from '../../assets/Projects/bondi.png'
+import ecommerce from '../../assets/Projects/e-commerce.png'
+import customize from '../../assets/Projects/page customize.png'
+import managmentManagment from '../../assets/Projects/products managment.png'
+import calc from '../../assets/Projects/proj-10.png'
+import weather from '../../assets/Projects/weather-site.png'
+import search from '../../assets/Projects/search (1).png'
+
+const Projects = () => {
+  return (
+    <div className='projects'>
+        <div className='projectsBoxes'>
+            <div className='box left'>
+                <div className='projectInformation'>
+                    <p>Featured Project</p>
+                    <h2 className='product-name'>ProjectName</h2>
+                    <p className='Project-desc'>
+                    A web app for visualizing personalized Spotify data. View your
+                    top artists, top tracks, recently played tracks, and detailed audio
+                    information about each track. Create and save new playlists of
+                    recommended tracks based on your existing playlists and more.
+                    </p>
+                    <div className='project-icons'>
+
+                    </div>
+                    <div className='btns-icons'>
+
+                    </div>
+                </div>
+                <div className='project-img'>
+                    <img src={edu} alt=''/>
+                    <div className='bg-project'></div>
+                </div>
+            </div>
+            <div className='box right'>
+            <div className='project-img'>
+                    <img src={bondi} alt=''/>
+                    <div className='bg-project right'></div>
+                </div>
+                <div className='projectInformation'>
+                    <p className='head-title right'>Featured Project</p>
+                    <h2 className='product-name right'>ProjectName</h2>
+                    <p className='Project-desc right'>
+                    A web app for visualizing personalized Spotify data. View your
+                    top artists, top tracks, recently played tracks, and detailed audio
+                    information about each track. Create and save new playlists of
+                    recommended tracks based on your existing playlists and more.
+                    </p>
+                    <div className='project-icons'>
+
+                    </div>
+                    <div className='btns-icons'>
+
+                    </div>
+                </div>
+            </div>
+            <div className='box left'>
+                <div className='projectInformation'>
+                    <p>Featured Project</p>
+                    <h2 className='product-name'>ProjectName</h2>
+                    <p className='Project-desc'>
+                    A web app for visualizing personalized Spotify data. View your
+                    top artists, top tracks, recently played tracks, and detailed audio
+                    information about each track. Create and save new playlists of
+                    recommended tracks based on your existing playlists and more.
+                    </p>
+                    <div className='project-icons'>
+
+                    </div>
+                    <div className='btns-icons'>
+
+                    </div>
+                </div>
+                <div className='project-img'>
+                    <img src={ecommerce} alt=''/>
+                    <div className='bg-project'></div>
+                </div>
+            </div>
+            <div className='box right'>
+            <div className='project-img'>
+                    <img src={customize} alt=''/>
+                    <div className='bg-project right'></div>
+                </div>
+                <div className='projectInformation'>
+                    <p className='head-title right'>Featured Project</p>
+                    <h2 className='product-name right'>ProjectName</h2>
+                    <p className='Project-desc right'>
+                    A web app for visualizing personalized Spotify data. View your
+                    top artists, top tracks, recently played tracks, and detailed audio
+                    information about each track. Create and save new playlists of
+                    recommended tracks based on your existing playlists and more.
+                    </p>
+                    <div className='project-icons'>
+
+                    </div>
+                    <div className='btns-icons'>
+
+                    </div>
+                </div>
+            </div>
+            <div className='box left'>
+                <div className='projectInformation'>
+                    <p>Featured Project</p>
+                    <h2 className='product-name'>ProjectName</h2>
+                    <p className='Project-desc'>
+                    A web app for visualizing personalized Spotify data. View your
+                    top artists, top tracks, recently played tracks, and detailed audio
+                    information about each track. Create and save new playlists of
+                    recommended tracks based on your existing playlists and more.
+                    </p>
+                    <div className='project-icons'>
+
+                    </div>
+                    <div className='btns-icons'>
+
+                    </div>
+                </div>
+                <div className='project-img'>
+                    <img src={managmentManagment} alt=''/>
+                    <div className='bg-project'></div>
+                </div>
+            </div>
+            <div className='box right'>
+            <div className='project-img'>
+                    <img src={calc} alt=''/>
+                    <div className='bg-project right'></div>
+                </div>
+                <div className='projectInformation'>
+                    <p className='head-title right'>Featured Project</p>
+                    <h2 className='product-name right'>Calculater</h2>
+                    <p className='Project-desc right'>
+                    A web app for visualizing personalized Spotify data. View your
+                    top artists, top tracks, recently played tracks, and detailed audio
+                    information about each track. Create and save new playlists of
+                    recommended tracks based on your existing playlists and more.
+                    </p>
+                    <div className='project-icons'>
+
+                    </div>
+                    <div className='btns-icons'>
+
+                    </div>
+                </div>
+            </div>
+            <div className='box left'>
+                <div className='projectInformation'>
+                    <p>Featured Project</p>
+                    <h2 className='product-name'>ProjectName</h2>
+                    <p className='Project-desc'>
+                    A web app for visualizing personalized Spotify data. View your
+                    top artists, top tracks, recently played tracks, and detailed audio
+                    information about each track. Create and save new playlists of
+                    recommended tracks based on your existing playlists and more.
+                    </p>
+                    <div className='project-icons'>
+
+                    </div>
+                    <div className='btns-icons'>
+
+                    </div>
+                </div>
+                <div className='project-img'>
+                    <img src={managmentManagment} alt=''/>
+                    <div className='bg-project'></div>
+                </div>
+            </div>
+            <div className='box right'>
+            <div className='project-img'>
+                    <img src={weather} alt=''/>
+                    <div className='bg-project right'></div>
+                </div>
+                <div className='projectInformation'>
+                    <p className='head-title right'>Featured Project</p>
+                    <h2 className='product-name right'>weather</h2>
+                    <p className='Project-desc right'>
+                    A web app for visualizing personalized Spotify data. View your
+                    top artists, top tracks, recently played tracks, and detailed audio
+                    information about each track. Create and save new playlists of
+                    recommended tracks based on your existing playlists and more.
+                    </p>
+                    <div className='project-icons'>
+
+                    </div>
+                    <div className='btns-icons'>
+
+                    </div>
+                </div>
+            </div>
+            <div className='box left'>
+                <div className='projectInformation'>
+                    <p>Featured Project</p>
+                    <h2 className='product-name'>search bar</h2>
+                    <p className='Project-desc'>
+                    A web app for visualizing personalized Spotify data. View your
+                    top artists, top tracks, recently played tracks, and detailed audio
+                    information about each track. Create and save new playlists of
+                    recommended tracks based on your existing playlists and more.
+                    </p>
+                    <div className='project-icons'>
+
+                    </div>
+                    <div className='btns-icons'>
+
+                    </div>
+                </div>
+                <div className='project-img'>
+                    <img src={search} alt=''/>
+                    <div className='bg-project'></div>
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Projects
