3e6cb44fee39a44dbef7cfd8e0e5f04d6da3822c
ashraf mohamed <ashrafmohamed1176@gmail.com>
2023-10-23 04:20:05 +0200

    add skills animation

--- src/pages/projects/Projects.jsx
+++ src/pages/projects/Projects.jsx
@@ -9,20 +9,39 @@ import weather from '../../assets/Projects/weather-site.png'
 import search from '../../assets/Projects/search (1).png'
 import translator from '../../assets/Projects/translator.png'
 
+import { FaHtml5 } from "react-icons/fa6";
+import css from '../../assets/Rectangle 7.png'
+import figma from '../../assets/Rectangle 1.png'
+import react from '../../assets/Rectangle 2.png'
+import js from '../../assets/Rectangle 6.png'
+import redux from '../../assets/Rectangle 8.png'
+import next from '../../assets/Rectangle 9.png'
+import git from '../../assets/git.svg'
+import cmd from '../../assets/terminal.svg'
+import githup from '../../assets/github.svg'
+import bootstarp from '../../assets/bootstrap.svg'
+import linux from '../../assets/linux.svg'
+import slack from '../../assets/slack.svg'
+import notion from '../../assets/notion.svg'
+import firebase from '../../assets/firebase.svg'
+
 const Projects = () => {
   return (
     <div className='projects'>
         <div className='projectsBoxes'>
             <div className='box left'>
                 <div className='projectInformation'>
+                    <div>
                     <p class="head-title">Featured Project</p>
                     <h2 className='product-name'>Ash-education</h2>
                     <p className='Project-desc'>
                     Specially designed for beginners because he is familiar with all the fields of technology of this era
                     Responsive design with all devices
                     </p>
+                    </div>
                     <div className='project-icons'>
-
+                        <FaHtml5 />
+                        <img src={css} alt='' />
                     </div>
                     <div className='btns-icons'>
 
