0b016adbe6d55906813e912c31cc439bb12a7094
AShraf Mohamed <124946844+ashrafmo-1@users.noreply.github.com>
2024-02-13 08:13:06 +0200

    links social

--- src/footer/Footer.jsx
+++ src/footer/Footer.jsx
@@ -1,6 +1,5 @@
 import './footer.css'
 import { FaFacebookF } from "react-icons/fa";
-import { FaInstagram } from "react-icons/fa";
 import { FiGithub } from "react-icons/fi";
 import { AiOutlineReddit } from "react-icons/ai";
 import { CiLinkedin } from "react-icons/ci";
@@ -10,10 +9,9 @@ const Footer = () => {
       <div className='top'></div>
       <div className='description'>
         <div className='social'>
-          <a href='./'> <FaFacebookF /></a>
-          <a href='./'> <FaInstagram /></a>
+          <a href='https://www.facebook.com/ASHRAFMoOoHAMED'> <FaFacebookF /></a>
           <a href='https://github.com/ashrafmo-1'><FiGithub /> </a>
-          <a href='./'> <AiOutlineReddit /></a>
+          <a href='https://www.reddit.com/user/ashrafmo_1/?onetap_auto=true'> <AiOutlineReddit /></a>
           <a href='https://www.linkedin.com/in/ashraf-mohamed-b931a7236/'><sapn> <CiLinkedin /> </sapn></a>
         </div>
         <div className='copy-right'>&copy; 2024 all rights reserved by, ashraf mohamed</div>
