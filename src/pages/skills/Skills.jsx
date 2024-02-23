89521903a562b9e7c07d05b4a8b19a74d3df90ef
AShraf Mohamed <124946844+ashrafmo-1@users.noreply.github.com>
2024-02-23 20:36:25 +0200

    edit skills style

--- src/pages/skills/Skills.jsx
+++ src/pages/skills/Skills.jsx
@@ -2,6 +2,7 @@ import html from "../../assets/html.png";
 import css from '../../assets/css-3.png';
 import sass from '../../assets/sass.png';
 import js from '../../assets/js.png';
+import ts from '../../assets/typescript.png'
 import react from '../../assets/science.png';
 import figma from '../../assets/figma.png';
 import redux from '../../assets/icons8-redux-48.png';
@@ -21,52 +22,70 @@ const Skills = () => {
     <h1 className='skillsTitle'>
       My favorite skills
     </h1>
-    <p>'</p>
     <div className='skills-boxes'>
-    <div className='box html'>
+    <div className='box'>
         <img src={html} alt='' />
+        <p className="title">HTML</p>
       </div>
       <div className='box'>
         <img src={css} alt='' />
+        <p className="title">css</p>
       </div>
       <div className='box'>
         <img src={sass} alt='' />
+        <p className="title">sass</p>
       </div>
       <div className='box'>
         <img src={bootstarp} alt='' />
+          <p className="title" style={{fontSize: '12px'}}>bootstarp</p>
       </div>
       <div className='box'>
         <img src={js} alt='' />
+          <p className="title">JS</p>
+      </div>
+      <div className='box'>
+        <img src={ts} alt='' />
+          <p className="title">TS</p>
       </div>
       <div className='box'>
         <img src={react} alt='' />
+          <p className="title">React</p>
       </div>
       <div className='box'>
         <img src={vite} alt='' />
+          <p className="title">Vite</p>
       </div>
       <div className='box'>
         <img src={next} className='next' alt='' />
+          <p className="title">Next</p>
       </div>
       <div className='box'>
         <img src={redux} alt='' />
+          <p className="title">Redux</p>
       </div>
       <div className='box'>
         <img src={figma} alt='' />
+          <p className="title">Figma</p>
       </div>
       <div className='box'>
         <img src={git} alt='' />
-      </div>
-      <div className='box'>
-        <img src={cmd} alt='' />
+          <p className="title">Git</p>
       </div>
       <div className='box'>
         <img src={githup} alt='' />
+          <p className="title">Github</p>
+      </div>
+      <div className='box'>
+        <img src={cmd} alt='' />
+          <p className="title">CMD</p>
       </div>
       <div className='box'>
         <img src={slack} alt='' />
+          <p className="title">Slack</p>
       </div>
       <div className='box'>
         <img src={notion} alt='' />
+          <p className="title">Notion</p>
       </div>
     </div>
     </div>
